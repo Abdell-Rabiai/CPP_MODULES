8433a732933553f66942c45776127167d286587a
Abdelouahed  Rabiai <arabiai@e2r5p4.1337.ma>
2024-06-14 19:13:13 +0100

    z

--- sortvec.cpp
+++ sortvec.cpp
@@ -65,7 +65,8 @@ void mergeInsertionSort(std::vector<int>& arr) {
 }
 
 int main() {
-    std::vector<int> vec = {78,-5,-1,0,45,3,2,1,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
+    // std::vector<int> vec = {78,-5,-1,0,45,3,2,1,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
+    std::vector<int> vec = {9,8,7,6,5,4,3,2,1,0};
 
     std::cout << "Original vector: ";
     for (const auto& elem : vec) {
